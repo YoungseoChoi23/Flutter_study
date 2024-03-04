a1597ec164e955c33aca9cffacd76ab086f5aa82
YoungseoChoi <dudtj9326@ewhain.net>
2024-03-04 17:13:47 +0900

    Map type 연습

--- 1. Dart/MapType.dart
+++ 1. Dart/MapType.dart
@@ -7,4 +7,27 @@ void main() {
     'Hermione Granger': '헤르미온느 그레인저',
   };
   print(dictionary);
+
+  Map<String, bool> isHarryPotter = {
+    'Harry Potter': true,
+    'Ron Weasley': true,
+    'Ironman': false,
+  };
+
+  print(isHarryPotter);
+  isHarryPotter.addAll({
+    'Spiderman': false,
+  });
+  print(isHarryPotter);
+
+  print(isHarryPotter['Ironman']);
+  isHarryPotter["Hulk"] = false;
+  print(isHarryPotter);
+  isHarryPotter["Spiderman"] = true;
+  print(isHarryPotter);
+  isHarryPotter.remove('Harry Potter');
+  print(isHarryPotter);
+
+  print(isHarryPotter.keys);
+  print(isHarryPotter.values);
 }
